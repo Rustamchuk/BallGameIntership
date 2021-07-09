145484227fffa56344e1a427ddb8f45a0a616811
Rustamchuk <hgnmkoujm@icoud.com>
2021-07-10 01:09:57 +0300

    Game

---
+++ Assets/Scripts/Player/PlayerLife.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerLife : MonoBehaviour
+{
+    public UnityEvent _died;
+    public CoinTakenEvent _coinTaken;
+
+    private int _purse;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Barrier barrier))
+        {
+            _died.Invoke();
+        }
+        else if (other.TryGetComponent(out Coin coin))
+        {
+            _purse++;
+            _coinTaken.Invoke(_purse);
+
+            Destroy(other.gameObject);
+        }
+    }
+}
+
+[System.Serializable]
+public class CoinTakenEvent : UnityEvent<int> { }
---
+++ Assets/Scripts/Restarter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restarter : MonoBehaviour
+{
+    [SerializeField] private string _menuScene;
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(_menuScene);
+    }
+}

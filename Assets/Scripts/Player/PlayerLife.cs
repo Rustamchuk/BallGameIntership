7f93a7e80906f30b1fd325974d7a238c24b5d610
Rustamchuk <hgnmkoujm@icloud.com>
2021-08-02 14:56:33 +0300

    Game

---
+++ Assets/Scripts/Player/PlayerLife.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerLife : MonoBehaviour
+{
+    public UnityEvent Died;
+    public CoinTakenEvent CoinTaken;
+
+    private int _purse;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Barrier barrier))
+        {
+            Died.Invoke();
+        }
+        else if (other.TryGetComponent(out Coin coin))
+        {
+            _purse++;
+            CoinTaken.Invoke(_purse);
+
+            Destroy(other.gameObject);
+        }
+    }
+}
+
+[System.Serializable]
+public class CoinTakenEvent : UnityEvent<int> { }
---
+++ Assets/Scripts/UI/Purse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Purse : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _value;
+
+    public void ShowPurse(int value)
+    {
+        _value.text = value.ToString();
+    }
+}
